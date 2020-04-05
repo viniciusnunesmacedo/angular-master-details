b74141cc49ef634cc842790185b7f88bd2c1638e
Vinicius Nunes Macedo <vnmacedo@hotmail.com>
2020-04-05 19:12:38 -0300

    adding EntryFormComponent

---
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -0,0 +1,134 @@
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { Entry } from "../shared/entry.model";
+import { EntryService } from "../shared/entry.service";
+
+import { switchMap } from "rxjs/operators";
+
+import toastr from "toastr";
+
+@Component({
+  selector: 'app-entry-form',
+  templateUrl: './entry-form.component.html',
+  styleUrls: ['./entry-form.component.css']
+})
+export class EntryFormComponent implements OnInit, AfterContentChecked {
+
+  currentAction: string;
+  entryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  entry: Entry = new Entry();
+
+  constructor(
+    private entryService: EntryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.setCurrentAction();
+    this.buildEntryform();
+    this.loadEntry();
+  }
+  
+  ngAfterContentChecked(){
+    this.setPageTitle();
+  }
+
+  submitForm(){
+    this.submittingForm = true;
+
+    if(this.currentAction == 'new'){
+      this.createEntry();
+    }else{
+      this.updateEntry();
+    }
+  }
+
+  // private methods
+  private setPageTitle() {
+    if(this.currentAction == 'new'){
+      this.pageTitle = "Cadastro Novo Lançamento"
+    }else{
+      const entryName = this.entry.name || "";
+      this.pageTitle = "Editando Lançamento: " + entryName;
+    }
+  }
+
+  private setCurrentAction() {
+    if(this.route.snapshot.url[0].path == 'new'){
+      this.currentAction = 'new';
+    }else{
+      this.currentAction = 'edit';
+    }
+  }
+  private buildEntryform() {
+    this.entryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null],
+      type: [null, [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      paid: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]],
+    })
+  }
+  
+  private loadEntry() {
+
+    if(this.currentAction == "edit"){
+
+      this.route.paramMap.pipe(
+        switchMap(params=> this.entryService.getById(+params.get("id")))
+      ).subscribe(
+        (entry) => {
+          this.entry = entry;
+          this.entryForm.patchValue(entry) // binds loaded entry data to EntryForm
+        },
+        (error) => alert("Ocorreu um erro no servidor. Tente novamente mais tarde. ")
+      )
+    }
+  }
+
+  private updateEntry() {
+    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+
+    this.entryService.update(entry).subscribe(
+      entry => this.actionForSuccess(entry),
+      error => this.actionForError(error)
+    )
+  }
+  private createEntry() {
+    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+
+    this.entryService.create(entry).subscribe(
+      entry => this.actionForSuccess(entry),
+      error => this.actionForError(error)
+    )
+  }
+  private actionForError(error: any): void {
+    toastr.error("Ocorreu um erro ao procesasr a sua solicitação.");
+
+    this.submittingForm = false;
+
+    if(error.status === 422){
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    }else{
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Tente mais tarde."];
+    }
+  }
+  private actionForSuccess(entry: Entry): void {
+    toastr.success("Solicitação processada com sucesso!");
+
+    // redirect/reload component page
+    this.router.navigateByUrl('entries', {skipLocationChange: true}).then(
+      () => this.router.navigate(["entries", entry.id, "edit"])
+    )
+  }
+}
