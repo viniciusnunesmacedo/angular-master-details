1fcd2eb0956c2e103fd81b76e0bb739f597ee437
Vinicius Nunes Macedo <vnmacedo@hotmail.com>
2020-04-05 09:30:35 -0300

    Improving category list component

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -5,12 +5,13 @@ import { Category } from  "./pages/categories/shared/category.model";
 export class InMemoryDatabase implements InMemoryDbService{
 
     createDb(){
-        const categories: Category = [
+        const categories: Category[] = [
             { id: 1, name: "Moradia", description: "Pagamentos das contas de casa." },
             { id: 2, name: "Saúde", description: "Plano de saúde e remédios." },
             { id: 3, name: "Lazer", description: "Cinemas, parque, praias, etc." },
             { id: 4, name: "Salário", description: "Recebimento de salário." },
             { id: 5, name: "Trabalho", description: "Trabalhos como freelancer." }
+            
         ]
 
         return { categories };
