2b292070d06cbe0ce31ef154ba4d29c121e136bd
Vinicius Nunes Macedo <vnmacedo@hotmail.com>
2020-04-08 16:48:55 -0300

    project restructuring

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDatabase } from "../in-memory-database";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+  ],
+  exports: [
+    // shared modules
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ]
+})
+export class CoreModule { }
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from "@angular/forms";
 
 import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
@@ -8,15 +6,15 @@ import { EntryFormComponent } from './entry-form/entry-form.component';
 
 import { CalendarModule } from "primeng/calendar";
 import { IMaskModule } from "angular-imask";
+import { SharedModule } from 'primeng/api/shared';
 
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
   imports: [
-    CommonModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
-    IMaskModule
-  ]
+    IMaskModule,
+    SharedModule
+  ],
 })
 export class EntriesModule { }
